d83da6dd5cdf4213a62a6b8f11d8e9d5d75e03c5
jprinz <jprinz@conestogac.on.ca>
2017-05-08 11:37:40 -0400

    Wired up new buttons

--- app/src/main/java/com/example/jprinz/helloandroid/MyActivity.java
+++ app/src/main/java/com/example/jprinz/helloandroid/MyActivity.java
@@ -2,6 +2,10 @@ package com.example.jprinz.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        // Let's take a look at Toast and Log in action
+        Toast.makeText(this, "Can you see me", Toast.LENGTH_SHORT).show();
+
+
+        Log.i("info", "Done creating the app");
+
+    }
+
+    public void topClick(View v){
+        Toast.makeText(this, "Top button clicked", Toast.LENGTH_SHORT).show();
+        Log.i("info", "The user clicked the top button");
+    }
+
+    public void bottomClick(View v){
+        Toast.makeText(this, "Bottom button clicked", Toast.LENGTH_SHORT).show();
+        Log.i("info", "The user clicked the top button");
+    }
+
+    public boolean onCreateOpationsMenu (Menu menu) {
+        return true;
     }
 }
